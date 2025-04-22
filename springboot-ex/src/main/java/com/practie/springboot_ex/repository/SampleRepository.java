4b4425c3e2d310180605acd071df35b04ca06b61
settipalli23 <mail2settipalli@gmail.com>
2025-04-22 09:16:01 +0530

    added new Spring boot code

---
+++ springboot-ex/src/main/java/com/practie/springboot_ex/repository/SampleRepository.java
@@ -0,0 +1,11 @@
+package com.practie.springboot_ex.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Repository
+public class SampleRepository {
+
+
+
+}
---
+++ springboot-ex/src/main/java/com/practie/springboot_ex/service/SampleService.java
@@ -0,0 +1,7 @@
+package com.practie.springboot_ex.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+}
